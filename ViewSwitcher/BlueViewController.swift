77ebc804b37d0c77b69e2573e7a40fe438f72d21
Nyarko Betrand <Betrand37@gmail.com>
2022-10-09 20:45:02 -0400

    Initial Commit

---
+++ ViewSwitcher/BlueViewController.swift
@@ -0,0 +1,38 @@
+//
+//  BlueViewController.swift
+//  ViewSwitcher
+//
+//  Created by Kobbie on 09/10/2022.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Blue View Button Pressed",
+                                      message: "You pressed the button on the blue view created by Betrand",
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Yes, I did", style: .default,
+                                   handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
